5e9d82f91417761d2d1f1c643ea56e94fe201f3a
Addison Dalton <zarch14@gmail.com>
2018-03-11 11:00:59 -0400

    Added basic html layout for the encounter page.

--- EotE_Encounter/Data/EncounterContext.cs
+++ EotE_Encounter/Data/EncounterContext.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EotE_Encounter.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EotE_Encounter.Data
 {
-    public class EncounterContext
+    public class EncounterContext : DbContext
     {
+        public EncounterContext(DbContextOptions<EncounterContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Character> Characters { get; set; }
     }
 }
