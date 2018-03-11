70d065c7fd2c5ba6b4cc495dff203966813a1e34
Addison Dalton <zarch14@gmail.com>
2018-03-11 10:20:17 -0400

    Added Character model

---
+++ EotE_Encounter/Models/Character.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EotE_Encounter.Models
+{
+    public class Character
+    {
+    }
+}
