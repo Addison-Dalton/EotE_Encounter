e5e458f0aa14bcfac6b3c504a4056fae45fcf053
Addison Dalton <zarch14@gmail.com>
2018-03-11 10:30:07 -0400

    Added Saved property to Character

--- EotE_Encounter/Models/Character.cs
+++ EotE_Encounter/Models/Character.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace EotE_Encounter.Models
 {
     public class Character
     {
+        public int Id { get; set; }
+        [Required][StringLength(250)]
+        public string Name { get; set; }
+        public byte Triumphs { get; set; }
+        public byte Succeses { get; set; }
+        public byte Advantages { get; set; }
+        public byte InitativeIndex { get; set; }
+        public bool Saved { get; set; }
+        [StringLength(1000)]
+        public string Notes { get; set; }
+
     }
 }
--- EotE_Encounter/Models/Encounter.cs
+++ EotE_Encounter/Models/Encounter.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using EotE_Encounter.Models;
 
 namespace EotE_Encounter.Models
 {
     public class Encounter
     {
+        //these first three properties may not be used just yet.
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public byte Round { get; set; }
+        public List<Character> CharactersInEncounter { get; set; }
+        [StringLength(2000)]
+        public string Notes { get; set; }
     }
 }
