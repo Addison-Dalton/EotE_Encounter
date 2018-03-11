ff5f38c41c34022b88fe5264691637887c807555
Addison Dalton <zarch14@gmail.com>
2018-03-11 14:31:38 -0400

    Added create character button and empty PartialView for create character form

---
+++ EotE_Encounter/Controllers/CharacterController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EotE_Encounter.Controllers
+{
+    public class CharacterController : Controller
+    {
+        public ActionResult CreateCharacter()
+        {
+            return PartialView("Add");
+        }
+    }
+}
--- EotE_Encounter/Controllers/HomeController.cs
+++ EotE_Encounter/Controllers/HomeController.cs
@@ -33,5 +33,11 @@ namespace EotE_Encounter.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public ActionResult CreateCharacter()
+        {
+            return PartialView("Add");
+        }
     }
 }
+
