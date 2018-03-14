da604596738b0b120125986ed4c9a188ed7a626d
Addison Dalton <zarch14@gmail.com>
2018-03-14 10:25:04 -0400

    Added Encounter prop to Character and updated database to reflect this.

--- EotE_Encounter/Models/Character.cs
+++ EotE_Encounter/Models/Character.cs
@@ -15,6 +15,7 @@ namespace EotE_Encounter.Models
         public byte Succeses { get; set; }
         public byte Advantages { get; set; }
         public short IniativeScore { get; set; } //this is calculated based on # of Triumphs, successes, and advantages
+        public Encounter Encounter { get; set; }
         public bool Saved { get; set; }
         [StringLength(1000)]
         public string Notes { get; set; }
@@ -33,22 +34,66 @@ namespace EotE_Encounter.Models
             character.IniativeScore = score;
         }
 
-        public void AddToEncounter(Encounter encounter, Character character)
+        public void AddToEncounter(Encounter encounter, Character newCharacter)
         {
+            //add character to list, then sort by iniativeScore
+            encounter.CharactersInEncounter.Add(newCharacter);
+            encounter.CharactersInEncounter.Sort((x, y) => x.IniativeScore.CompareTo(y.IniativeScore));
+
+            //MAY NOT NEED ANY OF THIS IF ABOVE CODE WORKS
+            /*
             //if this list is empty, add character
             if (!encounter.CharactersInEncounter.Any())
             {
-                encounter.CharactersInEncounter.Add(character);
+                encounter.CharactersInEncounter.Add(newCharacter);
             }
             else //if not empty, determine where to add character based on iniativescore
             {
                 foreach (Character characterInEncounter in encounter.CharactersInEncounter)
                 {
-                    if (character.IniativeScore > characterInEncounter.IniativeScore)
+                    int index = encounter.CharactersInEncounter.IndexOf(characterInEncounter);
+                    if (newCharacter.IniativeScore > characterInEncounter.IniativeScore)
                     {
-
+                        encounter.CharactersInEncounter.Insert(index, newCharacter);
+                        break;
+                    }else if (newCharacter.IniativeScore == characterInEncounter.IniativeScore) //if same score, choose one based on 50/50 chance for each
+                    {
+                        if(DetermineTie(newCharacter, characterInEncounter) == newCharacter.Id)
+                        {
+                            encounter.CharactersInEncounter.Insert(index, newCharacter);
+                            break;
+                        }
+                        else
+                        {
+                            if(index == encounter.CharactersInEncounter.Count() - 1) //add to end of list if reached end of list
+                            {
+                                encounter.CharactersInEncounter.Add(newCharacter);
+                            }
+                            else
+                            {
+                                encounter.CharactersInEncounter.Insert(index + 1, newCharacter);
+                            }
+                        }
+                    }
+                    else if (index == encounter.CharactersInEncounter.Count() - 1) //add to end of list if reached end of list
+                    {
+                        encounter.CharactersInEncounter.Add(newCharacter);
                     }
                 }
+            }*/
+        }
+
+        //returns the Id (int) of the character randomly chosen. Each character has 50/50 chance to be chosen.
+        private int DetermineTie(Character characterA, Character characterB)
+        {
+            Random random = new Random();
+            if(random.Next(1, 11) >= 5)
+            {
+                return characterA.Id;
+            }
+            else
+            {
+                return characterB.Id;
             }
         }
     }
---
+++ Eote_Encounter/Migrations/20180314141755_AddEncounterPropToCharacter.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace EotE_Encounter.Migrations
+{
+    public partial class AddEncounterPropToCharacter : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EncounterId",
+                table: "Characters",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Encounter",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Notes = table.Column<string>(maxLength: 2000, nullable: true),
+                    Round = table.Column<byte>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Encounter", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Characters_EncounterId",
+                table: "Characters",
+                column: "EncounterId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Characters_Encounter_EncounterId",
+                table: "Characters",
+                column: "EncounterId",
+                principalTable: "Encounter",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Characters_Encounter_EncounterId",
+                table: "Characters");
+
+            migrationBuilder.DropTable(
+                name: "Encounter");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Characters_EncounterId",
+                table: "Characters");
+
+            migrationBuilder.DropColumn(
+                name: "EncounterId",
+                table: "Characters");
+        }
+    }
+}
