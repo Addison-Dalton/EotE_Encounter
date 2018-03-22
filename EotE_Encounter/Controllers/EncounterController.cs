4ce8dc133bd6f30b679c1f3f4eefd7e019fd4466
Addison Dalton <zarch14@gmail.com>
2018-03-22 00:19:00 -0400

    Added functionality for moving characters up and down the initiative list

--- EotE_Encounter/Controllers/EncounterController.cs
+++ EotE_Encounter/Controllers/EncounterController.cs
@@ -47,5 +47,62 @@ namespace EotE_Encounter.Controllers
             encounter.Characters = encounter.Characters.OrderByDescending(c => c.IniativeScore).ToList();
             return PartialView("Details", encounter);
         }
+
+        /*changes the iniative order of the character passed. This is done by swapping the iniativeScore of the 
+         * character passed with either the character below or above it, which is passed via the direction variable.
+         * It the character is at the top of the list, and moved up then the character is moved to the bottom of the list.
+         * The opposite occurs if the character is at the bottom of the list.
+         */
+        public ActionResult ChangeInitiative(int characterId, string direction)
+        {
+            const string MOVE_UP = "up";
+            const string MOVE_DOWN = "down";
+            List<Character> characters = _context.Characters.OrderByDescending(c => c.IniativeScore).ToList();
+            Character movedCharacter = _context.Characters.Where(c => c.Id == characterId).SingleOrDefault();
+
+            //move character up
+            if(direction == MOVE_UP)
+            {
+                //detect if character is at the top of the list
+                if(movedCharacter.Id != characters.FirstOrDefault().Id)
+                {
+                    short tempInitiativeScore = movedCharacter.IniativeScore;
+                    int movedCharacterIndex = characters.IndexOf(movedCharacter);
+                    movedCharacter.IniativeScore = characters[movedCharacterIndex - 1].IniativeScore;
+                    characters[movedCharacterIndex - 1].IniativeScore = tempInitiativeScore;
+                }
+                else
+                {
+                    
+                    for(int i = 1; i < characters.Count; i++)
+                    {
+                        short tempInitiativeScore = movedCharacter.IniativeScore;
+                        movedCharacter.IniativeScore = characters[i].IniativeScore;
+                        characters[i].IniativeScore = tempInitiativeScore;
+                    }
+                }
+            }else if (direction == MOVE_DOWN)
+            {
+                if(movedCharacter.Id != characters.LastOrDefault().Id)
+                {
+                    short tempInitiativeScore = movedCharacter.IniativeScore;
+                    int movedCharacterIndex = characters.IndexOf(movedCharacter);
+                    movedCharacter.IniativeScore = characters[movedCharacterIndex + 1].IniativeScore;
+                    characters[movedCharacterIndex + 1].IniativeScore = tempInitiativeScore;
+                }
+                else
+                {
+                    for(int i = characters.Count - 2; i >= 0; i--)
+                    {
+                        short tempInitiativeScore = movedCharacter.IniativeScore;
+                        movedCharacter.IniativeScore = characters[i].IniativeScore;
+                        characters[i].IniativeScore = tempInitiativeScore;
+                    }
+                }
+            }
+
+            _context.SaveChanges();
+            return RedirectToAction("Details", new { encounterId = movedCharacter.EncounterId });
+        }
     }
 }
